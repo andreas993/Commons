b457fde67f6401dfbaf79a6c5d401794b9a868ac
Patrick Andreas Skarjot <patrick.andreas93@gmail.com>
2022-11-06 16:52:11 +0100

    "first commit"

---
+++ Common.WPF/UserControls/Card.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Common.WPF.UserControls
+{
+  public partial class Card : UserControl
+  {
+    public Card()
+    {
+      InitializeComponent();
+    }
+
+    public string Day
+    {
+      get { return (string)GetValue(DayProperty); }
+      set { SetValue(DayProperty, value); }
+    }
+
+    public static readonly DependencyProperty DayProperty =
+        DependencyProperty.Register("Day", typeof(string), typeof(Card));
+
+
+    public string Value
+    {
+      get { return (string)GetValue(ValueProperty); }
+      set { SetValue(ValueProperty, value); }
+    }
+
+    public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register("Value", typeof(string), typeof(Card));
+
+
+
+    public ImageSource Source
+    {
+      get { return (ImageSource)GetValue(SourceProperty); }
+      set { SetValue(SourceProperty, value); }
+    }
+
+    public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register("Source", typeof(ImageSource), typeof(Card));
+
+
+  }
+}
